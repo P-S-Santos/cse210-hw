bf002da70c3a6ffa2938881835af2c8d6255e994
P-S-Santos <rockyridgesaints@gmail.com>
2023-05-03 13:30:26 +0200

    Finished Week03 Prepare activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Hardware Engineer";
+        job1._company = "Macrohard";
+        job1._startYear = 2002;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2019;
+        job2._endYear = 2034;
+
+        Resume resume = new Resume();
+        resume._name = "Job Jobsman";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
