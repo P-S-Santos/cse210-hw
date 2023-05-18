f2b0ced81e88462746eb48123baec3c559bdef11
P-S-Santos <rockyridgesaints@gmail.com>
2023-05-18 22:51:22 +0200

    Finished W05 Prepare assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(5);
+        Fraction third = new Fraction(3,4);
+        Fraction fourth = new Fraction(1,3);
+        Console.WriteLine(first.GetFractionString()+"\n"+first.GetDecimalValue());
+        Console.WriteLine(second.GetFractionString()+"\n"+second.GetDecimalValue());
+        Console.WriteLine(third.GetFractionString()+"\n"+third.GetDecimalValue());
+        Console.WriteLine(fourth.GetFractionString()+"\n"+fourth.GetDecimalValue());
     }
 }
