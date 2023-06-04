3060630a70d4391eb43bc95119dbd0861f2f0dc2
P-S-Santos <rockyridgesaints@gmail.com>
2023-06-04 06:54:48 +0200

    Finished W07 Prepare assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment test1 = new Assignment("Jimmy Applesauce", "History");
+        Console.WriteLine(test1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment test2 = new MathAssignment("Jane Bananabread", "Algebra", "Section 4.2", "Problems 3-14");
+        Console.WriteLine(test2.GetSummary());
+        Console.WriteLine(test2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment test3 = new WritingAssignment("John Coconutpie", "Writing", "The History of Fruit Salad");
+        Console.WriteLine(test3.GetSummary());
+        Console.WriteLine(test3.GetWritingInfo());
     }
 }
